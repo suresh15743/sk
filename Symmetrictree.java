93fbfb2607b2b75289ae00a88bcd3c31dbd2cbea
suresh15743 <sureshbabu15743@gmail.com>
2025-06-11 12:13:43 +0530

    Symmetrictree.java

--- Symmetrictree.java
+++ Symmetrictree.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root == null) return false;
-        return checkTree(root.left,root.right);
+        return Symmetric(root.left,root.right);
     }
-    public static boolean checkTree(TreeNode left,TreeNode right)
+    public static boolean Symmetric(TreeNode left,TreeNode right)
     {
         if(left == null || right == null) return left == right;
         if(left.val != right.val) return false;
-        return checkTree(left.left,right.right) 
-               && checkTree(left.right,right.left);
+        return Symmetric(left.left,right.right) 
+               && Symmetric(left.right,right.left);
     }
 }
